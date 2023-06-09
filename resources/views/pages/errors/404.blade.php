baf04aa63d1254055f9ece69de6f78f6df120a0c
dreamer_ror <alipov050403andrey@gmail.com>
2023-06-09 12:36:23 +1000

    New 404 page

--- resources/views/pages/errors/404.blade.php
+++ resources/views/pages/errors/404.blade.php
@@ -1,5 +1,10 @@
-@extends('pages.layouts.app')
+@extends('pages.layouts.main')
 
 @section('content')
-    Страница, к которой вы обратились, не существует.
+    <header-pages
+        :bg_image="'/storage/bg/main-bg.jpg'"
+        :url="'{{ url('/') }}'"
+        :content="'<h1>Страница, к которой вы обратились, не существует</h1>'">
+
+    </header-pages>
 @endsection
